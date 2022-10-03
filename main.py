b5fc8c184ef961b309de687d7b50b749755881cc
kokushok <margaret.kisel@gmail.com>
2022-10-03 12:37:32 +0300

    Adjustments to the text

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import time
+
 print("Hello world!")
+print("Свободу обезьянам!")
+
+
+while True:
+    print("Never gonna give you up")
+    print("never gonna get let u down")
+    print("Never gonna run around and desert you")
+    print("Never gonna make you cry")
+    print("Never gonna say goodbye")
+    print("Never gonna tell a lie and hurt you")
+    time.sleep(5.0)
